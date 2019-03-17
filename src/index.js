2cf6b9f97d22f77c39e9f8d44f6bcd045b3476af
unknown <maildeal@gmail.com>
2019-03-17 13:52:38 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+function maxPrimeFactor(base) {
+    let result;
+    for(let i = 2; i <= base; i++){
+        result = [i];
+        while(base % i == 0) {
+            base = base / i;
+            result[1] ? result[1]++ : result[1] = 1; 
+        }
+    }
+    return result;
+}
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let [maxPrime, primeCount] = maxPrimeFactor(base);
+    let zeroCount = 0;
+    for (let i = maxPrime; i <= number; i = i * maxPrime){
+		zeroCount += Math.floor(number/i);
+    }
+    return Math.floor(zeroCount / primeCount);
 }
